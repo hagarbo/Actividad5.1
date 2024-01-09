1c767d5c5e6619dd31db38a81691e7f1349daaca
hagarbo <hagarbo@gmail.com>
2024-01-09 15:25:30 +0100

    feat: jugando con bootstrap - modo nocturno

--- login.php
+++ login.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Login</title>
 </head>
 
 <body>
-    <div class="container">
-        <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="container border border-3 rounded-4 mt-5  p-4" id="form-container">
+        <h1 class="h1 text-center pb-3 border-bottom">Inicio de sesión</h1>
+        <form class="row g-3 p-5 fs-5">
+            <div class="col-md-12 mb-3 form-floating">
+                <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="algo@example.com" required>
+                <label for="inputEmail">Correo Electrónico</label>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+            <div class="col-md-12 mb-3 form-floating">
+                <input type="password" class="form-control" id="inputPassword" placeholder="contraseña" required>
+                <label for="inputPassword">Contraseña</label>
             </div>
-            <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
-            <a href="register.php" class="btn btn-secondary">Regístrese aquí</a>
-        </form>
+            <div class="col-md-3 mb-3"></div>
+            <button type="submit" class="btn btn-lg btn-info fs-5 col-md-2 mx-auto mt-5 mb-5">Iniciar sesión</button>
+            <a href="register.php" class="btn btn-secondary btn-link fs-5 col-md-2 mx-auto mt-5 mb-5">Regístrese aquí</a>
+            <div class="col-md-3 mb-3"></div>
     </div>
+    </form>
     <div id="mensajes"></div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </html>
--- register.php
+++ register.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Register</title>
 </head>
 
@@ -40,24 +39,24 @@ if (isset(
 ?>
 
 <body>
-    <div class="container border border-3 rounded-4 mt-5 bg-light p-4" id="form-container">
-        <h1 class="h1">Registro de usuario</h1>
-        <form class="p-3">
-            <div class="mb-3">
-                <label for="inputEmail" class="form-label">Email:</label>
-                <input type="email" class="form-control" id="inputEmail" name="inputEmail" aria-describedby="emailHelp">
+    <div class="container border border-3 rounded-4 mt-5  p-4" id="form-container">
+        <h1 class="h1 text-center pb-3 border-bottom">Registro de usuario</h1>
+        <form class="row g-3 p-5 fs-5">
+            <div class="col-md-12 mb-3 form-floating">
+                <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="algo@example.com" required>
+                <label for="inputEmail">Correo Electrónico</label>
             </div>
-            <div class="mb-3">
-                <label for="inputPassword" class="form-label">Contraseña:</label>
-                <input type="password" class="form-control" id="inputPassword">
+            <div class="col-md-6 mb-3 form-floating">
+                <input type="password" class="form-control" id="inputPassword" placeholder="contraseña" required>
+                <label for="inputPassword">Contraseña</label>
             </div>
-            <div class="mb-3">
-                <label for="checkPassword" class="form-label">Repita contraseña:</label>
-                <input type="password" class="form-control" id="checkPassword">
+            <div class="col-md-6 mb-3 form-floating">
+                <input type="password" class="form-control" id="checkPassword" placeholder="contraseña" required>
+                <label for="checkPassword">Repita contraseña</label>
             </div>
-            <div class="mb-3">
+            <div class="col-md-4 mb-3">
                 <label class="form-label" for="inputRol">Seleccione el rol:</label>
-                <select id="inputRol" class="form-select" required>
+                <select id="inputRol" class="form-select fs-5" required>
                     <?php
                     foreach ($roles as $rol) {
                         echo "<option value=" . $rol['id'] . ">" . $rol['name'] . "</option>";
@@ -65,10 +64,17 @@ if (isset(
                     ?>
                 </select>
             </div>
-            <button type="submit" class="btn btn-primary">Registrar Usuario</button>
+            <div class="col-md-3 mb-3">
+
+            </div>
+
+            <button type="submit" class="btn btn-lg btn-info fs-5 col-md-3 mx-auto mt-5 mb-5">Registrar Usuario</button>
+
     </div>
     </form>
     <div id="mensajes"></div>
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </html>
